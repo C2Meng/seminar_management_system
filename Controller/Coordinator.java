f8b6f7afdf8225be863aa039ee4ab270aadf4ca6
C2Meng <chancm530@gmail.com>
2026-01-06 10:46:55 +0800

    added register user to coordinator.java

--- Controller/Coordinator.java
+++ Controller/Coordinator.java
@@ -2,12 +2,24 @@ package Controller;
 
 import InterfaceLib.Navigator;
 import InterfaceLib.Role;
+import InterfaceLib.SignUp;
+import Models.WriteToCSV;
 
-public class Coordinator extends User {
+
+public class Coordinator extends User implements SignUp {
 
     private int coordinatorID;
     private String name;
     private String faculty;
+
+    private String email;
+    private String password;
+
+    private String userType;
+    private WriteToCSV writeToCSV = new WriteToCSV();
+    private boolean isRegistered = false;
+    private String line;
+
    
     private Navigator navigator;
 
@@ -15,6 +27,27 @@ public class Coordinator extends User {
         super(email , name , password , Role.PROCOORDINATOR);
         this.navigator = navigator;
     }
+
+    @Override
+    public void registerUser(String email , String name , String password , String userType){
+         
+      
+         writeToCSV.getFilePath();
+         line = email + "," + name + "," + password + "," + userType;
+         isRegistered = true;
+         writeToCSV.writeData(line);
+         navigator.goTo("LoginPage");
+
+
+    }
+
+    @Override
+    public void deleteUser(String name, String userType) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    
 
     Seminar createSeminar(String title){
         return null;
@@ -28,6 +61,8 @@ public class Coordinator extends User {
     void nominateAward(Award winner){
         
     }
+
+    
 
     
 
